e4c0a4e14fa6579e1bbebf61dfed4a21998c6690
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2023-07-15 22:21:14 +0100

    Setup TUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use crossterm::{
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{backend::CrosstermBackend, widgets::Paragraph, Frame, Terminal};
+use std::{io::Stdout, time::Duration};
+
 fn main() {
-    println!("Hello World!")
+    let mut terminal = setup_terminal().unwrap();
+    run(&mut terminal).unwrap();
+    restore_terminal(&mut terminal).unwrap();
+}
+
+fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, anyhow::Error> {
+    let mut stdout = std::io::stdout();
+    enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen)?;
+    Ok(Terminal::new(CrosstermBackend::new(stdout))?)
+}
+
+fn restore_terminal(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+) -> Result<(), anyhow::Error> {
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    Ok(terminal.show_cursor()?)
+}
+
+fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), anyhow::Error> {
+    loop {
+        terminal.draw(render)?;
+        if event::poll(Duration::from_millis(250))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame<'_, CrosstermBackend<Stdout>>) {
+    frame.render_widget(Paragraph::new("Hello World"), frame.size());
 }
